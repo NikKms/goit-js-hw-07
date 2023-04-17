7aedc59105addabfabf762dc4a4816eb16af47db
Nikolay Kamyshnikov <119286614+NikKms@users.noreply.github.com>
2023-04-17 21:11:10 +0300

    first exs

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryRef = document.querySelector(".gallery");
+galleryRef.insertAdjacentHTML("beforeend", itemOfNewGallery(galleryItems));
+
+galleryRef.addEventListener("click", openModalWindow);
+
+function itemOfNewGallery(ref) {
+  return ref
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+         <a class="gallery__link" href="large-image.jpg">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`;
+    })
+    .join("");
+}
+
+function openModalWindow(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== "IMG") return;
+
+  const imageSrc = e.target.dataset.source;
+
+  const fullImage = basicLightbox.create(`
+    <img src=${imageSrc} width="1280" height="855">
+`);
+
+  fullImage.show();
+  document.addEventListener("keydown", (e) => closeModalWindow(e, fullImage));
+}
+
+function closeModalWindow(e, modalWinow) {
+  if (e.code === "Escape") {
+    modalWinow.close();
+    e.preventDefault;
+    document.removeEventListener("keydown", closeModalWindow);
+  }
+}
